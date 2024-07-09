f624384b882a8b190593a7f4d1dcc289ce0c34d4
Yoshiyuki Ito <thanks2music@gmail.com>
2024-07-09 19:19:27 +0900

    Add method to use query for fetching post details

--- front/repositories/PostRepository.ts
+++ front/repositories/PostRepository.ts
@@ -5,6 +5,21 @@ class PostRepository {
     static getList() {
         return Repository(WpGraphQlPostConst.list).getWp()
     }
+
+    // slugから記事単体を取得
+    static getOne({ id }: {
+        id: string
+    }) {
+        return Repository(
+            WpGraphQlPostConst.one,
+            { variables: { id } }
+        ).getWp()
+    }
+
+    // 全記事のslugを取得
+    static getAllSlugList() {
+        return Repository(WpGraphQlPostConst.allSlugList).getWp()
+    }
 }
 
 export default PostRepository
--- front/services/PostService.ts
+++ front/services/PostService.ts
@@ -29,6 +29,49 @@ class PostService {
             return []
         }
     }
+
+    // slugから記事単体を取得
+    static async getOne({ id }: {
+        id: string
+    }): Promise<PostType | null> { // エラーがあればnullを返す
+        try {
+            const res = await RepositoryFactory.post.getOne({ id })
+            const data = res.data.data.post
+            const post: PostType = {
+                id: data.id,
+                title: data.title,
+                slug: data.slug,
+                date: data.date,
+                content: data.content,
+                featuredImage: {
+                    url: data.featuredImage.node.sourceUrl
+                },
+                category: {
+                    slug: data.categories.edges[0].node.slug,
+                    name: data.categories.edges[0].node.name
+                }
+            }
+            return post // 配列ではなくPostTypeを返す
+        } catch {
+            return null
+        }
+    }
+
+    // 全記事のslugを取得
+    static async getAllSlugList(): Promise<{
+        params: {
+            slug: string
+        }
+    }[]> {
+        try {
+            const res = await RepositoryFactory.post.getAllSlugList()
+            return res.data.data.posts.edges.map((data: any) => {
+                return { params: { slug: data.node.slug } }
+            })
+        } catch {
+            return []
+        }
+    }
 }
 
 export default PostService
