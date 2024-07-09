25491007500720117ab3485ec6b89435c5296af9
Yoshiyuki Ito <thanks2music@gmail.com>
2024-07-09 19:18:10 +0900

    Add post detail query

--- front/constants/WpGraphQlConst.ts
+++ front/constants/WpGraphQlConst.ts
@@ -1,4 +1,6 @@
 export class WpGraphQlPostConst {
+    // 投稿一覧を取得するクエリ
+    // NOTE: https://github.com/vercel/next.js/blob/canary/examples/cms-wordpress/lib/api.ts
     static list = `query PostListQuery {
       posts {
         edges {
@@ -21,6 +23,41 @@ export class WpGraphQlPostConst {
             id
             slug
             title
+          }
+        }
+      }
+    }`
+
+    // slugから記事単体を持ってくる
+    static one = `query PostQuery($id: ID!) {
+      post(id: $id, idType: SLUG) {
+        categories {
+          edges {
+            node {
+              name
+              slug
+            }
+          }
+        }
+        date
+        content
+        featuredImage {
+          node {
+            sourceUrl
+          }
+        }
+        id
+        slug
+        title
+      }
+    }`
+
+    // 全記事のslugを持ってくる
+    static allSlugList = `query PostAllSlugListQuery {
+      posts(first: 10000) {
+        edges {
+          node {
+            slug
           }
         }
       }
