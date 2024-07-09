354b5f158e902af7a9de4e7872a9bd80596ba05f
Yoshiyuki Ito <thanks2music@gmail.com>
2024-07-09 14:45:13 +0900

    Add component files for Header, Footer, and Layout

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -7,21 +7,25 @@ import PostService from '../services/PostService'
 import usePostListSwr from '../hooks/swr/usePostListSwr'
 // component
 import PostBox from '../components/molecules/PostBox'
+import Link from 'next/link'
+import Layout from '../components/templates/Layout'
 
 const Home: NextPage<{
   staticPostList: PostType[]
 }> = ({ staticPostList }) => {
   const postList = usePostListSwr(staticPostList)
   return (
-    <div className='flex flex-wrap w-main mx-auto'>
-      {postList!.map((post) => {
-        return (
-          <div key={post.id} className='w-1/3 pr-4 pb-4 [&:nth-of-type(3n)]:pr-0'>
-            <PostBox post={post} />
-          </div>
-        )
-      })}
-    </div>
+    <Layout>
+      <div className='flex flex-wrap w-main mx-auto'>
+        {postList!.map((post) => {
+          return (
+            <div key={post.id} className='w-1/3 pr-4 pb-4 [&:nth-of-type(3n)]:pr-0'>
+              <PostBox post={post} />
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
   )
 }
 
