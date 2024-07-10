20bf9ca9182db7d5bb2a8062969a0afa66d47a35
Yoshiyuki Ito <thanks2music@gmail.com>
2024-07-10 10:21:16 +0900

    Add link to home in Header template

--- front/components/organisms/Header.tsx
+++ front/components/organisms/Header.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react'
+import Link from "next/link"
 // requires Tailwind CSS v2.0+
 import { Popover, Transition } from '@headlessui/react'
 import {
@@ -86,14 +87,14 @@ const Header = () => {
         <div className="border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className='flex justify-between items-center w-main mx-auto'>
             <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
-                <span className="sr-only">Workflow</span>
-                <img
-                    className="h-8 w-auto sm:h-10"
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                    alt=""
-                />
-                </a>
+                <Link href="/">
+                  <span className="sr-only">Workflow</span>
+                  <img
+                      className="h-8 w-auto sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                      alt=""
+                  />
+                </Link>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
