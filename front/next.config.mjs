886b3e3d98df413aab7b5521f70a0699692bab38
Yoshiyuki Ito <thanks2music@gmail.com>
2025-04-25 22:52:21 +0900

    Update config files

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,13 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: process.env.ALLOWED_IMAGE_HOST,
-      },
-    ],
-  },
+        hostname: process.env.ALLOWED_IMAGE_HOST
+      }
+    ]
+  }
 };
 
 export default nextConfig;
